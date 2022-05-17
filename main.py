c6613a5f471d5772ac79ed93aad0fd35b7aee51d
Haider A Arif <haider.arif@aol.com>
2022-05-17 12:54:51 +0100

    1

---
+++ main.py
@@ -0,0 +1,30 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+from datetime import datetime
+
+# def dt ():
+#     now = datetime.now()
+#     # print(now)
+#
+#     dt_string = now.strftime("%d%m%Y %H:%M:%S")
+#     print(dt_string)
+#
+# dt()
+from math import pi
+def cirAr (radius):
+    Area = pi * radius
+    print(Area)
+
+cirAr(1.1)
